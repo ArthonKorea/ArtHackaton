5c64c37f15803fc407d76d63b4eeb0bbb4ff4620
NamjinCho <skawls5028@gmail.com>
2017-07-15 12:12:02 +0900

    bleUpdate
    Update

--- app/src/main/java/xyz/namjin/arthon/MainActivity.java
+++ app/src/main/java/xyz/namjin/arthon/MainActivity.java
@@ -1,13 +1,63 @@
 package xyz.namjin.arthon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import xyz.namjin.arthon.R
+public class MainActivity extends AppCompatActivity implements BluetoothAdapter.LeScanCallback {
 
+    BluetoothManager btManager;
+    BluetoothAdapter btAdapter;
+    Button btn1;
+    TextView textView;
+    boolean flag=false;
+    public String mText="";
+    MainActivity myActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myActivity = this;
+        btManager = (BluetoothManager)getApplicationContext().getSystemService(BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        btAdapter.startLeScan(this);
+
+    }
+    public void onStart(){
+        super.onStart();
+        textView = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.btn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag)
+                {
+                    textView.setText(mText);
+                    btAdapter.startLeScan(myActivity);
+                    flag=false;
+                }else{
+                    flag = true;
+                    btAdapter.stopLeScan(myActivity);
+                }
+            }
+        });
+    }
+    @Override
+    public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+
+        Beacon beacon = Beacon.fromScanData(bytes,i);
+        if(beacon.getBeaconType()==0)
+        {
+            Log.d("Debuging",beacon.getProximityUuid());
+            mText+=beacon.getProximityUuid()+"\n";
+        }
+
     }
 }
